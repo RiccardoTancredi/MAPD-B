6175553378f1a4712b3c5a85b38ad502df6ae60d
RiccardoTancredi <ric.tancredi@gmail.com>
2023-04-15 17:07:09 +0200

    Update git-lazy.sh

--- git-lazy.sh
+++ git-lazy.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+cd /mnt/c/Users/ricta/Documents/MAPD-B
 git checkout '2023'
 git fetch upstream
 git merge upstream/'2023'
-
 git checkout development
 git pull
 git merge '2023'
